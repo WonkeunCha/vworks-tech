51d6d7307c3085acbe7ba813595175ba39076792
Cha Won Keun <aiden@vworks.tech>
2026-03-09 14:18:17 +0900

    fix: next.config.ts에서 next-intl/plugin 제거

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   images: {
@@ -21,4 +18,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
