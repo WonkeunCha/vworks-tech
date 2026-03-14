eca4c8aab1001c97c956fc4c0c32d1848e346492
Cha Won Keun <aiden@vworks.tech>
2026-03-14 16:16:44 +0900

    feat: 공지사항 모달 방식 상세보기 구현

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
+  images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_NOTION_TOKEN: process.env.NOTION_TOKEN,
+    NEXT_PUBLIC_NOTION_NOTICE_DB_ID: process.env.NOTION_NOTICE_DB_ID,
+    NEXT_PUBLIC_NOTION_FAQ_DB_ID: process.env.NOTION_FAQ_DB_ID,
+    NEXT_PUBLIC_NOTION_NEWS_DB_ID: process.env.NOTION_NEWS_DB_ID,
   },
 };
 
